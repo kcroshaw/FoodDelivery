8b1b23d0535a6a7048e0d051b41c618485098f5a
kcroshaw <kolbycroshaw@gmail.com>
2022-02-14 23:29:17 -0700

    SD Addition

---
+++ Infrastructure/Utilty/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Infrastructure.Utilty
+{
+    public class SD
+    {
+        public const string ManagerRole = "Manager";
+        public const string DriverRole = "Driver";
+        public const string KitchenRole = "Kitchen";
+        public const string CustomerRole = "Customer";
+    }
+
+}
+
