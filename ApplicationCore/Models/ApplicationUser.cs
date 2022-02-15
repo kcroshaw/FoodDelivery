4730bf8db12fec4a1a383457f65eb1b57ecf8b71
kcroshaw <kolbycroshaw@gmail.com>
2022-02-14 22:25:33 -0700

    Quick Fix

---
+++ ApplicationCore/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace ApplicationCore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+
+}
+
