1b4f1979601272ec54a759705fc6917bb0b56029
kcroshaw <kolbycroshaw@gmail.com>
2022-02-16 00:11:24 -0700

    Adding index pages

---
+++ FoodDelivery/Pages/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodDelivery.Pages.Admin.Users
+{
+	public class IndexModel : PageModel
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IndexModel(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+        public IEnumerable<ApplicationUser> applicationUsers { get; set; }
+        public Dictionary<string, List<string>> UserRoles { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public async Task OnGetAsync(bool success = false, string message = null)
+        {
+            Success = success;
+            Message = message;
+            UserRoles = new Dictionary<string, List<string>>();
+            applicationUsers = _unitOfWork.ApplicationUser.List();
+            foreach(var user in applicationUsers)
+            {
+                var userRole = await _userManager.GetRolesAsync(user);
+                UserRoles.Add(user.Id, userRole.ToList());
+            }
+        }
+    }
+}
