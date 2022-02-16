9dc988bc906b78d489f376c91f2b4ce86755f8bf
kcroshaw <kolbycroshaw@gmail.com>
2022-02-15 22:30:58 -0700

    Second Seeding Video

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using ApplicationCore.Models;
+using Infrastructure.Utilty;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +20,37 @@ namespace Infrastructure.Data
         public DbSet<MenuItem> MenuItem { get; set; }
 
         public DbSet<ApplicationUser> ApplicationUser { get; set;  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = SD.ManagerRole, NormalizedName = SD.ManagerRole.ToUpper() });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7211", Name = SD.CustomerRole, NormalizedName = SD.CustomerRole.ToUpper() });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7212", Name = SD.KitchenRole, NormalizedName = SD.KitchenRole.ToUpper() });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7213", Name = SD.DriverRole, NormalizedName = SD.DriverRole.ToUpper() });
+
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                    UserName = "Admin@admin.com",
+                    Email = "Admin@admin.com",
+                    NormalizedEmail = "ADMIN@ADMIN.COM",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    NormalizedUserName = "ADMIN@ADMIN.COM",
+                    PasswordHash = hasher.HashPassword(null, "Pa$$w0rd")
+                });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                    UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+                });
+        }
     }
 }
