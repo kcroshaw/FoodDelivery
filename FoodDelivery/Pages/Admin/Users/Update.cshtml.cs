96ee303f74ba9b857e6d00c95d968a44ff0c7381
kcroshaw <kolbycroshaw@gmail.com>
2022-02-16 00:31:24 -0700

    Adding html pages

---
+++ FoodDelivery/Pages/Admin/Users/Update.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodDelivery.Pages.Admin.Users
+{
+	public class UpdateModel : PageModel
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UpdateModel(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [BindProperty]
+        public ApplicationUser AppUser { get; set; }
+        public List<string> UsersRoles { get; set; }
+        public List<string> AllRoles { get; set; }
+        public List<string> OldRoles { get; set; }
+
+        public async Task OnGet(string id)
+        {
+            AppUser = _unitOfWork.ApplicationUser.Get(u => u.Id == id);
+            var roles = await _userManager.GetRolesAsync(AppUser);
+            UsersRoles = roles.ToList();
+            OldRoles = roles.ToList();
+            AllRoles = _roleManager.Roles.Select(r => r.Name).ToList();
+        }
+    }
+}
