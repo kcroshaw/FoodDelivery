43de196a1b57184d4da872bf8f83ddc15c9ec64c
Kolby Croshaw <kolbycroshaw@Croshaws-Mac-mini.local>
2022-02-23 20:28:13 -0700

    Ready for migration

---
+++ ApplicationCore/Models/OrderHeader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationCore.Models
+{
+	public class OrderHeader
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int UserId { get; set; }
+
+		[ForeignKey("UserId")]
+        public virtual ApplicationUser ApplcationUser { get; set; }
+
+		[Required]
+		public DateTime OrderDate { get; set; }
+
+		[Required]
+		[Display(Name = "Order Total")]
+		[DisplayFormat(DataFormatString = "{0:C}")]
+		public double OrderTotal { get; set; }
+
+		[Required]
+		[Display(Name = "Delivery Time")]
+		public DateTime DeliveryTime { get; set; }
+
+		[Required]
+		public DateTime DeliveryDate { get; set; }
+
+		public string Status { get; set; }
+
+		[Display(Name = "Delivery Name")]
+		public string DeliveryName { get; set; }
+
+		[Display(Name = "Phone Number")]
+		public string PhoneNumber { get; set; }
+
+        public string TransactionsId { get; set; }
+	}			
+}
+
+
+
