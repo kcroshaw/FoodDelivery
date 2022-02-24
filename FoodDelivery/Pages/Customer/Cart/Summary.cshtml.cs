c7e6cf8c96f0b0eea6b0579daee1a0d4083f05b0
Kolby Croshaw <kolbycroshaw@Croshaws-Mac-mini.local>
2022-02-23 23:54:04 -0700

    Ready for submission

--- FoodDelivery/Pages/Customer/Cart/Summary.cshtml.cs
+++ FoodDelivery/Pages/Customer/Cart/Summary.cshtml.cs
@@ -9,6 +9,7 @@ using FoodDelivery.ViewModels;
 using Infrastructure.Utilty;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 
 namespace FoodDelivery.Pages.Customer.Cart
 {
@@ -51,13 +52,70 @@ namespace FoodDelivery.Pages.Customer.Cart
             }
         }
 
-        //public IActionResult OnPost(string stripeToken)
-        //{
-        //    var claimsIdentity = (ClaimsIdentity)User.Identity;
-        //    var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        public IActionResult OnPost(string stripeToken)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-        //    OrderDetailsCart.ListCart = _unitOfWork.ShoppingCart.List(c => c.ApplicationUserId == claim.Value).ToList();
-        //    OrderDetailsCart.OrderHeader.
-        //}
+            OrderDetailsCart.ListCart = _unitOfWork.ShoppingCart.List(c => c.ApplicationUserId == claim.Value).ToList();
+            OrderDetailsCart.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+            OrderDetailsCart.OrderHeader.OrderDate = DateTime.Now;
+            OrderDetailsCart.OrderHeader.UserId = claim.Value;
+            OrderDetailsCart.OrderHeader.Status = SD.StatusSubmitted;
+            OrderDetailsCart.OrderHeader.DeliveryTime = Convert.ToDateTime(OrderDetailsCart.OrderHeader.DeliveryDate.ToShortDateString() + " " + OrderDetailsCart.OrderHeader.DeliveryTime.ToShortTimeString());
+
+            List<OrderDetails> orderDetailsList = new List<OrderDetails>();
+            _unitOfWork.OrderHeader.Add(OrderDetailsCart.OrderHeader);
+            _unitOfWork.Commit();
+
+            foreach(var item in OrderDetailsCart.ListCart)
+            {
+                item.MenuItem = _unitOfWork.MenuItem.Get(m => m.Id == item.MenuItemId);
+                OrderDetails orderDetails = new OrderDetails
+                {
+                    MenuItemId = item.MenuItemId,
+                    OrderId = OrderDetailsCart.OrderHeader.Id,
+                    Name = item.MenuItem.Name,
+                    Price = item.MenuItem.Price,
+                    Count = item.Count
+                };
+
+                OrderDetailsCart.OrderHeader.OrderTotal += (orderDetails.Count * orderDetails.Price) * (1 + SD.SalesTaxPercent);
+                _unitOfWork.OrderDetails.Add(orderDetails);
+            }
+
+            OrderDetailsCart.OrderHeader.OrderTotal = Convert.ToDouble(String.Format("{0:.##}", OrderDetailsCart.OrderHeader.OrderTotal));
+
+            HttpContext.Session.SetInt32(SD.ShoppingCart, 0);
+            _unitOfWork.Commit();
+
+            //calling stripe pop up api through JS
+
+            if(stripeToken != null)
+            {
+                var options = new ChargeCreateOptions
+                {
+                    Amount = Convert.ToInt32(OrderDetailsCart.OrderHeader.OrderTotal * 100), //requires in cents
+                    Currency = "usd",
+                    Description = "Order Id: " + OrderDetailsCart.OrderHeader.Id,
+                    Source = stripeToken
+                };
+
+                var service = new ChargeService();
+                Charge charge = service.Create(options);
+                OrderDetailsCart.OrderHeader.TransactionsId = charge.Id;
+
+                if(charge.Status.ToLower() == "succeeded")
+                {
+                    OrderDetailsCart.OrderHeader.PaymentStatus = SD.PaymentStatusApproved;
+                }
+                else
+                {
+                    OrderDetailsCart.OrderHeader.PaymentStatus = SD.PaymentStatusRejected;
+                }
+            }
+            _unitOfWork.Commit();
+            return RedirectToPage("/Customer/Cart/Orderconfirmation", new { id = OrderDetailsCart.OrderHeader.Id });
+        }
     }
 }
