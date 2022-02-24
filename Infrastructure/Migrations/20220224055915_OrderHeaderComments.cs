c2389f3379d062b8f4fdba147eaa238eefdf11f0
kcroshaw <kolbycroshaw@gmail.com>
2022-02-23 22:59:45 -0700

    Comments migration

---
+++ Infrastructure/Migrations/20220224055915_OrderHeaderComments.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class OrderHeaderComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "OrderHeader",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "526fc13b-e42b-474e-bceb-33b34b31ebe2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                column: "ConcurrencyStamp",
+                value: "edb47a3c-31fd-490c-8407-d853b7e300f4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7212",
+                column: "ConcurrencyStamp",
+                value: "05546f9b-7949-4e87-93f7-cab06c627d79");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7213",
+                column: "ConcurrencyStamp",
+                value: "8df59fe3-9108-4ab2-bf61-22e89b14959e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2aaf72d0-aea9-4992-b47e-e386ee813837", "AQAAAAEAACcQAAAAEHmh7hqaFMqxtMLEr6k1azxJPzm9mzJ3Cl9SSdFH/vUGYGQzpvzNwhEWLBG435pmxw==", "a3e63493-ce6d-41f8-983e-00fdd68b82b1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "OrderHeader");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                column: "ConcurrencyStamp",
+                value: "d73d39be-40e4-4a66-8772-edc1ff2e16fd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                column: "ConcurrencyStamp",
+                value: "f61d41b8-645c-405f-a873-d8f02b9ea9ce");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7212",
+                column: "ConcurrencyStamp",
+                value: "aa205fee-ec1c-4d09-a5c2-f8af9db710b6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2c5e174e-3b0e-446f-86af-483d56fd7213",
+                column: "ConcurrencyStamp",
+                value: "46fe697e-08b9-43f5-b036-a0948140dd51");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "de084d56-9fa0-4f25-a16e-651577e07929", "AQAAAAEAACcQAAAAEAphDEbw9/VrJ2FQzwXMMIuVVC2wgSAjhkxCOODN23WHNeQTvsgeHhHLzgpuGvKunQ==", "4773dd43-b6bb-43f2-b4b7-9b4dcdb71ec6" });
+        }
+    }
+}
