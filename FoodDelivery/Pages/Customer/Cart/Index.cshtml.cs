247a44467491bf0c3e69b1ebc6c28f1033ca0079
Kolby Croshaw <kolbycroshaw@Croshaws-Mac-mini.local>
2022-02-23 21:18:03 -0700

    Work on shopping cart page

---
+++ FoodDelivery/Pages/Customer/Cart/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using FoodDelivery.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodDelivery.Pages.Customer.Cart
+{
+	public class IndexModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public IndexModel(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public OrderDetailsCartVM OrderDetailsCart { get; set; }
+
+        public void OnGet()
+        {
+            OrderDetailsCart = new OrderDetailsCartVM()
+            {
+                OrderHeader = new OrderHeader(),
+                ListCart = new List<ShoppingCart>()
+            };
+
+            OrderDetailsCart.OrderHeader.OrderTotal = 0;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim != null)
+            {
+                IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.List(c => c.ApplicationUserId == claim.Value);
+                if(cart != null)
+                {
+                    OrderDetailsCart.ListCart = cart.ToList();
+                }
+
+                foreach(var cartList in OrderDetailsCart.ListCart)
+                {
+                    cartList.MenuItem = _unitOfWork.MenuItem.Get(n => n.Id == cartList.MenuItemId);
+                    OrderDetailsCart.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+                }
+            }
+        }
+    }
+}
---
+++ FoodDelivery/ViewModels/OrderDetailsCartVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ApplicationCore.Models;
+
+namespace FoodDelivery.ViewModels
+{
+	public class OrderDetailsCartVM
+	{
+		public OrderHeader OrderHeader { get; set; }
+		public List <ShoppingCart> ListCart { get; set; }
+	}
+}
+
